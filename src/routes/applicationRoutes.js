5d274cf63fce4753b3b527e9e132d680d18f790d
chamberezigbo <chamberezigbo@gmail.com>
2025-04-15 14:15:25 +0100

    added change password for dashborad v1

--- src/routes/applicationRoutes.js
+++ src/routes/applicationRoutes.js
@@ -19,7 +19,7 @@ const {
 const router = express.Router();
 
 router.post("/apply/:jobId", authenticateUser, applyForJob);
-router.post(
+router.get(
   "/applications/my-applications",
   authenticateUser,
   validateGetApplications,
